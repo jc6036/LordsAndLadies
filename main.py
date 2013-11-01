7ca680ab9534b8d8c30d26a3467d96fa2dd29472
jc6036 <jc6036@gmail.com>
2013-11-01 03:06:13 -0400

    Added mainfile. Added to TODO.

---
+++ main.py
@@ -0,0 +1,35 @@
+########################
+#Lords and Ladies      #
+#v0.0.a1               #
+#Authored by jc6036    #
+#Python 3.2 with       #
+#Tkinter, ttk and      #
+#Random modules        #
+########################
+
+from random import randint
+from tkinter import *
+import ttk
+
+class Kingdom(object):
+  """Contains the kingdoms"""
+  
+  def __init__(self):
+    pass
+  
+
+class Location(Kingdom):
+  """Various functions for locations in kingdoms"""
+  
+  def __init__(self):
+    pass
+
+
+class ImportantPerson(object):
+  """Standard template for a person"""
+  
+  def __init__(self):
+    pass
+
+
+
