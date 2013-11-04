52c76fc7cd3dd892d4fed8dc4132517b03e94b81
jc6036 <jc6036@gmail.com>
2013-11-03 23:13:40 -0500

    Added framework for main logic.

--- main.py
+++ main.py
@@ -11,29 +11,6 @@ from random import randint
 from tkinter import *
 import ttk
 
-def get_name(name_type): 
-  if name_type == "kingdom":
-    with open("placeholder", -r) as opened_file:
-      lines = opened_file.readlines()
-      return lines[randint(0, 201)]
-
-  elif name_type == "location":
-    with open("placeholder", -r) as opened_file:
-      lines = opened_file.readlines()
-      return lines[randint(0, 201)]
-
-  elif name_type == "person":
-    with open("placeholder", -r) as opened_file:
-      lines = opened_file.readlines()
-      return lines[randint(0, 201)]
-#Use this for random names at creation of place or person.
-
-
-def create_base_kingdom(name):
-  name = Kingdom(get_name("kingdom"))
-#Creates the basic Kingdom. Expand using functions.Kingdom.
-
-
 class Kingdom(object, name):
   """Contains the kingdoms and fucntions to populate."""
   
@@ -71,12 +48,12 @@ class Kingdom(object, name):
     
     identifier = InfluentialPerson(get_name("person"), chosen_job)
   
-  def create_location(self, identifier):
-    places = ["village", "town", "city", "castle"]
-    chosen_place = places[randint(0, 5)]
+  def create_locations(self, identifier):
+    places = ["village", "town", "city"]
+    chosen_place = places[randint(0, 4)]
     
     identifier = Location(get_name("location"), chosen_place)
-#For creation of Kingdoms/Populatio/Locations.
+#For creation of Kingdoms/Population/Locations.
   
 
 class Location(Kingdom, name, variation):
@@ -86,7 +63,7 @@ class Location(Kingdom, name, variation):
   def __init__(self, name, variation):
     name = self.name
     variation = self.variation
-#Location creator.
+#Location functions.
 
 
 class Person(object, name, name_type):
@@ -130,9 +107,98 @@ class InfluentialPerson(Person, name, job):
 
 
 
+def get_name(name_type): 
+  if name_type == "kingdom":
+    with open("placeholder", -r) as opened_file:
+      lines = opened_file.readlines()
+      return lines[randint(0, 201)]
+
+  elif name_type == "location":
+    with open("placeholder", -r) as opened_file:
+      lines = opened_file.readlines()
+      return lines[randint(0, 201)]
+
+  elif name_type == "person":
+    with open("placeholder", -r) as opened_file:
+      lines = opened_file.readlines()
+      return lines[randint(0, 201)]
+#Use this for random names at creation of place or person.
 
 
-#For main logic loop: figure out algorithm to generate
-#multiple people with different identifiers for
-#the ability to have more than one object of the same
-#type generated automatically.
+def create_kingdom(kingdom_number):
+#All-in-one function to both create and populate a kingdom.
+
+  check = raw_input("Type 'random', or input your own name.")
+  
+  if check == "random":
+    kingdom_number = Kingdom(get_name("kingdom"))
+    
+  else:
+    kingdom_number = Kingdom(check)
+  kingdom_number.populate_king_queen()
+  
+
+  print("The king of your kingdom is ", kingdom_one.king, ".")
+  print("The queen of your kingdom is ", kingdom_one.queen, ".")
+  print("Now we have to populate it.")
+  
+
+  number_of_children = raw_input("How many noble children will there be?")
+
+
+  if number_of_children > 10:
+    print("No more than ten.")
+    
+  elif number_of_children <= 10:
+    for i in range(1, number_of_children):
+      kingdom_number.populate_noble_children("child_" + str(i))
+      
+  else:
+    print("Input a number please.")
+    
+
+
+  number_of_landlords = raw_input("How many lords and ladies will ther be?")
+
+
+  if number_of_landlords > 20:
+    print("No more than twenty.")
+    
+  elif number_of_landlords <= 20:
+    for i in range(1, number_of_landlords + 1):
+      kingdom_number.populate_landlords("landlord_" + str(i))
+      
+  else:
+    print("Numbers only, please.")
+
+
+  number_of_locations = raw_input("How many locations? These are Villages, cities, and towns.")
+
+    
+  if number_of_locations > 20:
+    print("No more than twenty.")
+    
+  elif number_of_locations >= 20:
+    for i in range(1, number_of_locations + 1):
+      kingdom_number.create_locations("location_" + str(i))
+
+  else:
+    print("Numbers only, please.")
+
+
+print("""
+      Welcome to Lords and Ladies!\n
+      Prepare yourself for drama. Wars, crazy weather, and assassination abound!\n
+      """)
+
+print("""
+      
+      Create your first kingdom to get started.
+      """)
+
+create_kingdom("kingdom_1")
+
+amount_new = raw_input("Now then, how many more Kingdoms would you like to add?")
+
+for i in range(2, amount_new + 1):
+  create_kingdom("kingdom_" + str(i))
